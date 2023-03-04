3f023714817e806ecb403a8557a7ebfbb8fa6ca7
majidmahmoodlhr <57787986+majidmahmoodlhr@users.noreply.github.com>
2023-03-04 13:36:50 +0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+
 path <- getwd()
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url, file.path(path, "dataFiles.zip"))
